054dc4dcab73bfb0f6f6e693e70bb40e884221eb
Emilia Ruști <emilia.rusti17@gmail.com>
2025-05-29 13:27:17 +0300

    Adding design for the first page

---
+++ Pages/CompanyA_Simulation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectSAP.Pages
+{
+    public class CompanyA_SimulationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
